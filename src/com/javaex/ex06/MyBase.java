d123666add54c404ae8c1cf109e9a692f82631bb
kangsb-010313 <kangsb668@naver.com>
2025-05-09 10:41:58 +0900

    pt06 250509 10:41

--- src/com/javaex/ex06/MyBase.java
+++ src/com/javaex/ex06/MyBase.java
@@ -3,5 +3,23 @@ package com.javaex.ex06;
 public class MyBase extends Base{
 
     //코드작성
-    
+	public void service(String state) {
+		if(state.equals("낮")) {
+			day();
+		}else if(state.equals("오후")) {
+			afternoon();
+		}else {
+			night();
+		}
+	}
+    public void day() {
+    	System.out.println("낮에는 열심히 수업듣자");
+    }
+    public void night() {
+    	super.night();
+    }
+    public void afternoon() {   	
+    	System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
+    }
+
 }
--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -5,11 +5,22 @@ public class SmartPhone extends MusicPhone {
     public void execute(String str){
         
         //코드작성
-        
+        if("앱".equals(str)) {
+        	app();
+        }else if("음악".equals(str)) {
+        	playMusic();
+        }else {
+        	super.execute(str);
+        }
     }
  
     //메소드작성
-    
+    public void app() {
+    	System.out.println("앱실행");
+    }
+    public void playMusic() {
+    	System.out.println("다운로드해서 음악재생");
+    }
     //메소드작성
     
     
